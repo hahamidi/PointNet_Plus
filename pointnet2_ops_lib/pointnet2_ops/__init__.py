fb389372d8dea1260754fe5635d82e035859e318
hasanhamidi <hamidi3031@gmail.com>
2022-04-30 14:08:19 +0430

    first commit

---
+++ pointnet2_ops_lib/pointnet2_ops/__init__.py
@@ -0,0 +1,3 @@
+from pointnet2_ops_lib.pointnet2_ops import pointnet2_modules
+from  pointnet2_ops_lib.pointnet2_ops import pointnet2_utils
+from _version import __version__
---
+++ train.py
@@ -0,0 +1,68 @@
+import os
+import sys
+
+pointnet2_dir = os.path.split(os.path.abspath(__file__))[0]
+main_dir = "/".join(pointnet2_dir.split("/")[0:-1])
+pointnet2_ops_lib_dir = main_dir+"/pointnet2_ops_lib/" 
+
+sys.path.insert(0,main_dir)
+sys.path.insert(0,pointnet2_ops_lib_dir)
+
+import hydra
+import omegaconf
+import pytorch_lightning as pl
+import torch
+from pytorch_lightning.loggers import TensorBoardLogger
+# from surgeon_pytorch import Inspect,get_layers
+
+torch.backends.cudnn.enabled = True
+torch.backends.cudnn.benchmark = True
+
+
+def hydra_params_to_dotdict(hparams):
+    def _to_dot_dict(cfg):
+        res = {}
+        for k, v in cfg.items():
+            if isinstance(v, omegaconf.DictConfig):
+                res.update(
+                    {k + "." + subk: subv for subk, subv in _to_dot_dict(v).items()}
+                )
+            elif isinstance(v, (str, int, float, bool)):
+                res[k] = v
+
+        return res
+
+    return _to_dot_dict(hparams)
+
+
+@hydra.main("config/config.yaml")
+def main(cfg):
+    model = hydra.utils.instantiate(cfg.task_model, hydra_params_to_dotdict(cfg))
+    print(cfg)
+    
+    # early_stop_callback = pl.callbacks.EarlyStopping(patience=5)
+    # checkpoint_callback = pl.callbacks.ModelCheckpoint(
+    #     monitor="val_acc",
+    #     mode="max",
+    #     save_top_k=2,
+    #     filepath=os.path.join(
+    #         cfg.task_model.name, "{epoch}-{val_loss:.2f}-{val_acc:.3f}"
+    #     ),
+    #     verbose=True,
+    # )
+    # trainer = pl.Trainer(
+    #     gpus=list(cfg.gpus),
+    #     max_epochs=cfg.epochs,
+    #     early_stop_callback=early_stop_callback,
+    #     checkpoint_callback=checkpoint_callback,
+    #     distributed_backend=cfg.distrib_backend
+    # )
+
+    # print(get_layers(model))
+    # # trainer.fit(model)
+    # # trainer.test(model)
+    
+
+
+if __name__ == "__main__":
+    main()

f5f1e8dbc44dd2d3f456b3ca5a45504b2fd6aee2
hamidi3031@gmail.com <hasanhamidi>
2022-05-15 21:05:43 +0430

    1

--- train2.py
+++ train2.py
@@ -206,6 +206,8 @@ class Trainer:
         # print(target[0].size())
         # print(out[0])
         # print(target[0])
+        print((out[0].T).cpu().detach().numpy())
+        print(target[0].cpu().detach().numpy())
         show_embeddings((out[0].T).cpu().detach().numpy(),target[0].cpu().detach().numpy())
         self.training_loss.append(np.mean(train_losses))
         # self.training_acc.append(np.mean(train_acc))
